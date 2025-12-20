7ead5984a3c54f05486e0e90d0ca8496407ab571
AbdullahAmer7 <anoshe781@gmail.com>
2025-12-20 12:25:00 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,166 @@
+const translate = {
+  en: {
+    Home: "Home",
+    ExploreSites: "Explore Sites",
+    BookTickets: "BookTickets",
+    Guides: 'Guides',
+    Transportation: "Transportation",
+    Contact: "Contact",
+    English: "English",
+    Arabic: "Arabic",
+    Login: "Login",
+    DiscoverEgyptIslamicHeritage: "Discover Egypt's Islamic Heritage",
+    Bookguidedtourstotimelesslandmarks: "Book guided tours to timeless landmarks",
+    ExploreNow: "Explore Now",
+    FamousIslamicHeritageSites: "Famous Islamic Heritage Sites",
+    AlHakimMosque: "Al-Hakim Mosque",
+    p1: " The construction of the mosque began during the reign of the Fatimid Caliph Al-Aziz Billah in 380 AH/990 AD, and was completed by his son, Caliph Al-Hakim bi-Amr Allah, in 403 AH/1013 AD, It follows the layout of congregational mosques, It consists of a central open courtyard surrounded by four porticos, which are structural supports holding arches that support the wooden roof.",
+    SaladinCitadel: "Saladin Citadel",
+    p2: "Saladin began its construction between 572 AH/1176 AD and 579 AH/1183 AD, but it was not completed during his lifetime, It was completed by Sultan Al-Kamil ibn Al-Adil in 604 AH / 1207 AD, and it remained the seat of power in Egypt until the reign of Khedive Ismail.",
+    AlAzharMosque: "Al-Azhar Mosque",
+    p3: "It was built by Jawhar al-Siqilli, and the first Friday prayer in Ramadan was held there in the year 361 AH/972 AD. It was named Al-Azhar after Lady Fatima al-Zahra, Its initial layout consisted of an open courtyard surrounded by three porticos. Renovations, additions, and restorations continued during the Fatimid period and in the eras that followed.",
+    SultanAlZahirBaybarsMosque: "Sultan Al-Zahir Baybars Mosque",
+    p4: "It is considered one of the oldest mosques of the Mamluk era. Its construction was begun by Al-Zahir Baybars Al-Bunduqdari (658-676 AH / 1260-1277 AD) On an area of approximately three feddans (12,600 square meters), in the year 665 AH / 1266 AD, and it continued to host religious rituals for a long period.",
+    GayerAndersonMuseum: "Gayer-Anderson Museum",
+    p5: "The Gayer-Anderson Museum is one of the most important Islamic monuments in Egypt, distinguished by its oriental architectural design. The museum was originally composed of two separate houses, built during the Ottoman era, adjacent to each other.",
+    MuseumofIslamicArt: "Museum of Islamic Art",
+    p6: "The Museum of Islamic Art is the largest museum of Islamic antiquities in the world, displaying diverse collections of Islamic art from Egypt, India, China, Andalusia, the Levant, the Arabian Peninsula, North Africa, and other parts of the Islamic world. The idea of establishing it began during the reign of Khedive Ismail in 1869.",
+    SultanAlGhuriAgency: "Sultan Al-Ghuri Agency",
+    p7: "The Museum of Islamic Art is the largest museum of Islamic antiquities in the world, displaying diverse collections of Islamic art from Egypt, India, China, Andalusia, the Levant, the Arabian Peninsula, North Africa, and other parts of the Islamic world. The idea of establishing it began during the reign of Khedive Ismail in 1869.",
+    MrsWasilashouse: "Mrs. Wasila's house",
+    p8: "It is a historical house located in the Al-Darb Al-Ahmar area, near the Al-Azhar Mosque in Cairo. The house dates back to the Mamluk era and is known for its distinctive architecture and intricate decorations.",
+    RawdaIslandNilometer: "Rawda Island Nilometer",
+    p9: "The Nilometer on Rawda Island is an ancient structure used to measure the water level of the Nile River. It is located on Rawda Island in Cairo and dates back to the Islamic era. The Nilometer played a crucial role in determining the annual flood levels of the Nile, which was essential for agriculture and taxation purposes.",
+    AlJeddawiAgency: "Al-Jeddawi Agency",
+    p10: "Al-Jeddawi Agency is a historic commercial building located in Cairo, Egypt. It was established during the Ottoman era and served as a hub for trade and commerce. The agency was known for its vibrant market atmosphere, where merchants from various regions would gather to buy and sell goods.",
+    QaitbayCitadel: "Qaitbay Citadel",
+    p11: "It was built by Sultan Al-Ashraf Abu Al-Nasr Qaitbay Al-Mahmudi between 882 AH/1477 AD and 884 AH/1479 AD. The citadel is located in the Anfoushi district- Formerly Pharos Island – West Alexandria, consisting of a rectangular area surrounded by the sea on three sides. The citadel has a main entrance on the southwest side in the form of two towers. ",
+    JamalalDinhouse: "Jamal al-Din house",
+    p12: "The golden entity of Jamal al-Din “Shahbandar” the Great in Egypt in the year 1047 AH/ 1637 AD, located in Khashqadam Alley (Hawsh Qadam) Branching off from Al-Muizz li-Din Allah Street in the Al-Ghuriya area of Downtown Cairo, The house is considered one of Egypt's significant historical buildings",
+    Formore: "For more",
+    p13: " Experience the beauty of Islamic heritage in Egypt, where history lives in every stone and every street tells a timeless story.",
+    h5:"links",
+    Home:"Home",
+    OurServices:"Our Services",
+    Portfolio:"Portfolio",
+    Testimonials:"Testimonials",
+    Support:"Support",
+    TermsandCondition:"Terms and Condition",
+    AboutUs:"About Us",
+    SIgnIn:"Sign In",
+    Register:"Register",
+    Blog:"Blog",
+    ContactUs:"Contact Us",
+    P14:"Get in touch with us via mail phone.We are waiting for your call or message",
+    P15:"Phone: +20 (123) 456-7890",
+    Readmore: "Read more",
+    Booknow: "Book now",
+  },
+  ar: {
+    Home: "الصفحة الرئيسية",
+    ExploreSites: "استكشف المواقع",
+    BookTickets: "احجز التذاكر",
+    Guides: 'المرشدون',
+    Transportation: "المواصلات",
+    Contact: "اتصل بنا",
+    English: "الانجليزية",
+    Arabic: "العربية",
+    Login: "تسجيل دخول",
+    DiscoverEgyptIslamicHeritage: "اكتشف التراث الإسلامي في مصر",
+    Bookguidedtourstotimelesslandmarks: "احجز جولات إرشادية لأهم المعالم الخالدة",
+    ExploreNow: "استكشف الآن",
+    FamousIslamicHeritageSites: "أشهر مواقع التراث الإسلامي",
+    AlHakimMosque: "مسجد الحاكم بأمر الله",
+    p1: "بدأ بناء المسجد في عهد الخليفة الفاطمي العزيز بالله في عام 380 هـ / 990 م، واكتمل على يد ابنه الخليفة الحاكم بأمر الله في عام 403 هـ / 1013 م، ويتبع تصميم المساجد الجامعية، ويتكون من ساحة مركزية مفتوحة تحيط بها أربعة أروقة، وهي دعائم هيكلية تحمل أقواسًا تدعم السقف الخشبي.",
+    SaladinCitadel: "قلعة صلاح الدين",
+    p2: "بدأ صلاح الدين بناءها بين عامي 572 هـ / 1176 م و 579 هـ / 1183 م، لكنها لم تكتمل خلال حياته، واكتمل بناؤها على يد السلطان الكامل بن العادل في عام 604 هـ / 1207 م، وظلت مقر السلطة في مصر حتى عهد الخديوي إسماعيل.",
+    AlAzharMosque: "جامع الأزهر",
+    p3: "بُني على يد جوهر الصقلي، وأقيمت فيه أول صلاة جمعة في رمضان في عام 361 هـ / 972 م. سُمي الأزهر نسبة إلى السيدة فاطمة الزهراء، وكان تصميمه الأولي يتكون من ساحة مفتوحة تحيط بها ثلاثة أروقة. استمرت عمليات الترميم والإضافات خلال الفترة الفاطمية وفي العصور التي تلتها.",
+    SultanAlZahirBaybarsMosque: "مسجد السلطان الظاهر بيبرس",
+    p4: "يُعتبر من أقدم مساجد العصر المملوكي. بدأ بناؤه على يد الظاهر بيبرس البندقداري (658-676 هـ / 1260-1277 م) على مساحة تقارب ثلاثة فدادين (12,600 متر مربع)، في عام 665 هـ / 1266 م، واستمر في استضافة الطقوس الدينية لفترة طويلة.",
+    GayerAndersonMuseum: "متحف جايير أندرسون",
+    p5: "يُعد متحف جايير أندرسون من أهم المعالم الإسلامية في مصر، ويتميز بتصميمه المعماري الشرقي. كان المتحف في الأصل مكونًا من منزلين منفصلين، بُنيا خلال العصر العثماني، متجاورين مع بعضهما البعض.",
+    MuseumofIslamicArt: "متحف الفن الإسلامي",
+    p6: "متحف الفن الإسلامي هو أكبر متحف للآثار الإسلامية في العالم، يعرض مجموعات متنوعة من الفن الإسلامي من مصر والهند والصين والأندلس وبلاد الشام وشبه الجزيرة العربية وشمال أفريقيا وأجزاء أخرى من العالم الإسلامي. بدأت فكرة إنشائه خلال عهد الخديوي إسماعيل في عام 1869.",
+    SultanAlGhuriAgency: "وكالة السلطان الغوري",
+    p7: "هو مبنى تجاري كان يُستخدم لإيواء التجار القادمين، لتخزين وبيع البضائع، ولإجراء الصفقات التجارية. تقع وكالة السلطان الغوري في شارع الطبلية بمنطقة الأزهر وتُعرف أيضًا بوكالة النخلة والمصبغة.",
+    MrsWasilashouse: "بيت السيدة وسيلة",
+    p8: "يعود تاريخ بنائه إلى العصر العثماني، على يد الأخوين عبد الحق و لطفي محمد الكناني في عام 1074 هـ / 1664 م، وكانت السيدة وسيلة خاتون آخر ساكنيه منذ بداية القرن التاسع عشر الميلادي. يطل مدخل البيت على زقاق الست وسيلة.",
+    RawdaIslandNilometer: "رَوْضة الجزيرة – النيلوميتر",
+    p9: "بُني في عام 247 هـ / 861 م بأمر من الخليفة العباسي المتوكل على الله على جزيرة الروضة، وكان يُستخدم لقياس مستوى الفيضان، مما يسمح بتحديد كمية المياه الواردة أثناء الفيضان.",
+    AlJeddawiAgency: "وكالة الجدّاوي",
+    p10: "بُنيت على يد حسن بك الجدّاوي في عام 1207 هـ / 1792 م، الذي سُميت باسمه لأنه تولى إمارة جدة في عام 1184 هـ / 1770 م، وتُعتبر وكالة الجدّاوي دليلًا على الازدهار الاقتصادي الذي شهدته مدينة إسنا خلال القرن 12 هـ / 18 م.",
+    QaitbayCitadel: "قلعة قايتباي",
+    p11: "بُنيت على يد السلطان الأشرف أبو النصر قايتباي المحمودي بين عامي 882 هـ / 1477 م و 884 هـ / 1479 م. تقع القلعة في منطقة الأنفوشي - سابقًا جزيرة فاروس - غرب الإسكندرية، وتتكون من مساحة مستطيلة تحيط بها البحر من ثلاث جهات. للقلعة مدخل رئيسي على الجانب الجنوبي الغربي على شكل برجين.",
+    JamalalDinhouse: "بيت جمال الدين",
+    p12: "الكيان الذهبي لجمال الدين “شاهبندر” الكبير في مصر في عام 1047 هـ / 1637 م، يقع في زقاق خشقدم (حوش قدم) المتفرع من شارع المعز لدين الله في منطقة الغورية بوسط القاهرة، ويُعتبر المنزل من المباني التاريخية الهامة في مصر",
+    Formore: "لمزيد من المعلومات",
+    p13: "اكتشف جمال التراث الإسلامي في مصر، حيث يعيش التاريخ في كل حجر وكل شارع يروي قصة خالدة.",
+    h5:"الروابط",
+    Home:"الصفحة الرئيسية",
+    OurServices:"خدماتنا",
+    Portfolio:"أعمالنا",
+    Testimonials:"الشهادات",
+    Support:"الدعم",
+    TermsandCondition:"الشروط والأحكام",
+    AboutUs:"معلومات عنا",
+    SIgnIn:"تسجيل الدخول",
+    Register:"تسجيل",
+    Blog:"المدونة",
+    ContactUs:"اتصل بنا",
+    P14:"تواصل معنا عبر البريد أو الهاتف. نحن في انتظار مكالمتك أو رسالتك",
+    P15:"الهاتف: +٢٠(١٢٣)٤٥٦-٧٨٩٠",
+    Readmore: "اقرأ المزيد",
+    Booknow: "احجز الآن",
+  }
+}
+const langSelect = document.querySelector("select")
+langSelect.addEventListener('change', (event) => {
+  setLang(event.target.value);
+  localStorage.setItem("lang", event.target.value);
+
+})
+//    // لو فيه ترجمه محفوظه في localStorage يرجعه
+// document.addEventListener("DOMContentLoaded",()=>{
+//     const lang = localStorage.getItem("lang");
+//     setLang(localStorage.getItem("lang"));    
+// });
+const setLang = (lang) => {
+  const elements = document.querySelectorAll("[data-translate]");
+  elements.forEach((element) => {
+    const translateKey = element.getAttribute("data-translate");
+    element.textContent = translate[lang][translateKey];
+  });
+  if (lang === "ar") {
+    document.dir = "rtl";
+  } else {
+    document.dir = "ltr";
+  }
+}
+
+const toggleBtn = document.getElementById("darkToggle");
+const body = document.body;
+
+//   // لو فيه مود محفوظ في localStorage يرجعه
+//   if (localStorage.getItem("theme") === "dark") {
+//     body.classList.add("dark-mode");
+//     toggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+//     toggleBtn.classList.remove("btn-outline-dark");
+//     toggleBtn.classList.add("btn-outline-light");
+//   }
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    toggleBtn.classList.remove("btn-outline-dark");
+    toggleBtn.classList.add("btn-outline-light");
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    toggleBtn.classList.remove("btn-outline-light");
+    toggleBtn.classList.add("btn-outline-dark");
+  }
+});
